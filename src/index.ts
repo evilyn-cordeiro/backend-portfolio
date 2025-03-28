dbc399e371bec26964b2ec861ae28fd5252e115d
evilyn-cordeiro <evilyndeveloper@gmail.com>
2025-03-28 13:59:51 -0300

    feat: tratamento de idioma

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import cors from "cors";
 import { createClient } from "@supabase/supabase-js";
 import dotenv from "dotenv";
 import nodemailer from "nodemailer";
-import validator from "validator"; // Para validação do e-mail
+import validator from "validator";
 
 dotenv.config();
 
@@ -13,9 +13,6 @@ const supabaseKey = process.env.SUPABASE_KEY as string;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 const app = express();
-
-console.log("SUPABASE_URL:", process.env.SUPABASE_URL);
-console.log("SUPABASE_KEY:", process.env.SUPABASE_KEY);
 
 app.use(
   cors({
@@ -28,8 +25,28 @@ app.use(bodyParser.json());
 
 const PORT = process.env.PORT || 3000;
 
-// Função para enviar o e-mail com o currículo
-async function sendEmail(to: string, nome: string) {
+const messages: any = {
+  en: {
+    missingFields: "The fields 'name', 'email' and 'company' are required.",
+    invalidEmail: "The provided email is invalid.",
+    success: "Data saved and email sent successfully!",
+    emailSubject: "Curriculum Request | Evilyn Cordeiro",
+    emailText:
+      "Dear {name},\n\nI hope this message finds you well.\n\nPlease find attached my resume for your review.\n\nIf you have any questions or need further information, feel free to reach out.\n\nBest regards,\nEvilyn Cordeiro",
+    errorSavingData: "Error saving data or sending email.",
+  },
+  pt: {
+    missingFields: "Os campos 'nome', 'email' e 'empresa' são obrigatórios.",
+    invalidEmail: "O e-mail fornecido é inválido.",
+    success: "Dados salvos e e-mail enviado com sucesso!",
+    emailSubject: "Solicitação de Currículo | Evilyn Cordeiro",
+    emailText:
+      "Olá {name},\n\nEspero que esta mensagem o encontre bem.\n\nSegue em anexo o meu currículo para sua análise.\n\nSe tiver alguma dúvida ou precisar de mais informações, fique à vontade para entrar em contato.\n\nAtenciosamente,\nEvilyn Cordeiro",
+    errorSavingData: "Erro ao salvar dados ou enviar o e-mail.",
+  },
+};
+
+async function sendEmail(to: string, nome: string, idioma: string) {
   const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
@@ -38,15 +55,21 @@ async function sendEmail(to: string, nome: string) {
     },
   });
 
+  const attachmentPath =
+    idioma === "en"
+      ? "./public/cv-evilyndev-en.pdf"
+      : "./public/curriculo-evilyndev.pdf";
+
   const mailOptions = {
     from: process.env.EMAIL_USER,
     to: to,
-    subject: "Curriculum Request | Evilyn Cordeiro",
-    text: `Dear ${nome},\n\nI hope this message finds you well.\n\nPlease find attached my resume for your review.\n\nIf you have any questions or need further information, feel free to reach out.\n\nBest regards,\nEvilyn Cordeiro`,
+    subject: messages[idioma].emailSubject,
+    text: messages[idioma].emailText.replace("{name}", nome),
     attachments: [
       {
-        filename: "curriculo-evilyndev.pdf",
-        path: "./public/curriculo-evilyndev.pdf",
+        filename:
+          idioma === "en" ? "cv-evilyndev-en.pdf" : "curriculo-evilyndev.pdf",
+        path: attachmentPath,
       },
     ],
   };
@@ -56,33 +79,29 @@ async function sendEmail(to: string, nome: string) {
     console.log("E-mail enviado com sucesso!");
   } catch (error) {
     console.error("Erro ao enviar e-mail:", error);
-    throw new Error("Erro ao enviar o e-mail.");
+    throw new Error(messages[idioma].errorSavingData);
   }
 }
 
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
-});
-
 app.post("/api/form", async (req: Request, res: Response) => {
-  const { nome, email, empresa } = req.body;
+  const { nome, email, empresa, idioma = "pt" } = req.body;
 
-  // Validação de entrada
+  const validLanguages = ["pt", "en"];
+  const selectedIdioma = validLanguages.includes(idioma) ? idioma : "pt";
+
   if (!nome || !email || !empresa) {
     return res.status(400).json({
-      message: "Os campos 'nome', 'email' e 'empresa' são obrigatórios.",
+      message: messages[selectedIdioma].missingFields,
     });
   }
 
-  // Validação de e-mail
   if (!validator.isEmail(email)) {
     return res.status(400).json({
-      message: "O e-mail fornecido é inválido.",
+      message: messages[selectedIdioma].invalidEmail,
     });
   }
 
   try {
-    // Salvar os dados no Supabase
     const { data, error } = await supabase
       .from("data_visitants")
       .insert([{ nome, email, empresa }])
@@ -92,18 +111,21 @@ app.post("/api/form", async (req: Request, res: Response) => {
       throw error;
     }
 
-    // Enviar o e-mail com o currículo após salvar os dados
-    await sendEmail(email, nome);
+    await sendEmail(email, nome, selectedIdioma);
 
     return res
       .status(201)
-      .json({ message: "Dados salvos e e-mail enviado com sucesso!", data });
+      .json({ message: messages[selectedIdioma].success, data });
   } catch (error: any) {
     console.error("Erro ao salvar dados ou enviar o e-mail:", error.message);
     return res
       .status(500)
-      .json({ message: "Erro ao salvar dados ou enviar o e-mail." });
+      .json({ message: messages[selectedIdioma].errorSavingData });
   }
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
 
 export default app;
--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
-  process.env.SUPABASE_URL!, // Coloque o URL do Supabase
-  process.env.SUPABASE_KEY! // Coloque a chave da API do Supabase
+  process.env.SUPABASE_URL!,
+  process.env.SUPABASE_KEY!
 );
 
 export { supabase };
